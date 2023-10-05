2f7e27270d101e57934764c7510083eb0bfe8922
tsukimizake <shomasd@gmail.com>
2023-10-05 16:53:11 +0900

    small refactor on parser

--- lua/telescope-just/parser/init.lua
+++ lua/telescope-just/parser/init.lua
@@ -7,10 +7,9 @@ end
 function M.parse_line(line)
   local name, description = line:match("^(.-)#(.*)$")
   if name == nil and line ~= nil then
-    -- if there is no description, just return as is
-    line = trim(line) or line
-    line = line:gsub(" .*", "") or line
-    return line, "no desc"
+    -- if there is no description
+    name = line
+    description = "no desc"
   end
   name = trim(name) or name
   name = name:gsub(" .*", "") or name
