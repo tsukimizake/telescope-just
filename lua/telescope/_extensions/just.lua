b7fa73f1d7329f025b0f1865da17787c34be79b1
tsukimizake <shomasd@gmail.com>
2023-10-05 11:05:29 +0900

    or str on match

--- lua/telescope/_extensions/just.lua
+++ lua/telescope/_extensions/just.lua
@@ -18,12 +18,12 @@ local function parse_line(line)
   local name, description = line:match("^(.-)#(.*)$")
   if name == nil and line ~= nil then
     -- if there is no description, just return as is
-    line = trim(line)
-    line = line:gsub(" .*", "")
+    line = trim(line) or line
+    line = line:gsub(" .*", "") or line
     return line, "no desc"
   end
-  name = trim(name)
-  name = name:gsub(" .*", "")
+  name = trim(name) or name
+  name = name:gsub(" .*", "") or name
   return name, trim(description)
 end
 
